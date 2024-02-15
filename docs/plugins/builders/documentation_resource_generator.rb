87ad7cd1727214658fdf195d876802b2fd647293
Konnor Rogers <konnor5456@gmail.com>
2024-02-15 01:51:48 -0500

    implicit titles

--- docs/plugins/builders/documentation_resource_generator.rb
+++ docs/plugins/builders/documentation_resource_generator.rb
@@ -4,6 +4,7 @@ class Builders::DocumentationResourceGenerator < SiteBuilder
     hook :resources, :post_init do |resource|
       next if resource.collection.label != "documentation"
       resource.data = resource.data.merge({
+        "title" => resource_to_title(resource),
         "permalink" => resource_to_permalink(resource)
       })
     end
@@ -11,5 +12,9 @@ class Builders::DocumentationResourceGenerator < SiteBuilder
 
   def resource_to_permalink(resource)
     resource.relative_path.to_s.split(File::Separator).slice(1..-1).join("/").gsub(/\/\d+-/, "/").gsub(".md", "") + "/"
+  end
+
+  def resource_to_title(resource)
+    File.basename(resource.relative_path, ".*").gsub(/\/\d+-/, "").split(/-/).map(&:capitalize).join(" ")
   end
 end
